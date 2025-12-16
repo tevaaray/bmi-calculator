fbde07cd92f663f0e7e0892c9812a95302214d4e
tevaaray <241313@astanait.edu.kz>
2025-12-16 12:16:17 +0500

    BMI calculator project

--- app.js
+++ app.js
@@ -2,23 +2,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+
 const app = express();
 const PORT = 3000;
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
+
 app.post("/calculate-bmi", (req, res) => {
   const weight = parseFloat(req.body.weight);
   const height = parseFloat(req.body.height);
 
+
   if (weight <= 0 || height <= 0 || isNaN(weight) || isNaN(height)) {
     return res.send("<h2>Invalid input. Please enter positive numbers.</h2><a href='/'>Go back</a>");
   }
+
 
   const bmi = weight / (height * height);
   let category = "";
@@ -39,7 +45,7 @@ app.post("/calculate-bmi", (req, res) => {
   }
 
   res.send(`
-    <div style="text-align:center; margin-top:50px;">
+    <div style="text-align:center; margin-top:90px;">
       <h1>Your BMI Result</h1>
       <h2 style="color:${color};">BMI: ${bmi.toFixed(2)} (${category})</h2>
       <a href="/">Calculate Again</a>
